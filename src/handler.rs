28d96102c53ca23c81ad5e839a805757d7d12a7a
Mario Zupan <mario@zupzup.org>
2021-02-23 17:43:44 +0100

    add all handlers

--- src/handler.rs
+++ src/handler.rs
@@ -1,10 +1,40 @@
-use crate::{Clients, WebResult};
+use crate::{Clients, FasterClients, WebResult};
 use std::time::Duration;
 use warp::{reply, Reply};
 
 pub async fn read_handler(clients: Clients) -> WebResult<impl Reply> {
     let clients_lock = clients.lock().await;
-    // let clients_lock = clients.read().await;
+    let user_ids: Vec<String> = clients_lock
+        .iter()
+        .map(|(_, client)| client.user_id.to_string())
+        .collect();
+    tokio::time::sleep(Duration::from_millis(50)).await;
+    let result = user_ids
+        .iter()
+        .rev()
+        .map(|user_id| user_id.parse::<usize>().expect("can be parsed to usize"))
+        .fold(0, |acc, x| acc + x);
+    Ok(reply::html(result.to_string()))
+}
+
+pub async fn faster_read_handler(clients: Clients) -> WebResult<impl Reply> {
+    let clients_lock = clients.lock().await;
+    let user_ids: Vec<String> = clients_lock
+        .iter()
+        .map(|(_, client)| client.user_id.to_string())
+        .collect();
+    drop(clients_lock);
+    tokio::time::sleep(Duration::from_millis(50)).await;
+    let result = user_ids
+        .iter()
+        .rev()
+        .map(|user_id| user_id.parse::<usize>().expect("can be parsed to usize"))
+        .fold(0, |acc, x| acc + x);
+    Ok(reply::html(result.to_string()))
+}
+
+pub async fn fast_read_handler(clients: FasterClients) -> WebResult<impl Reply> {
+    let clients_lock = clients.read().await;
     let user_ids: Vec<String> = clients_lock
         .iter()
         .map(|(_, client)| client.user_id.to_string())
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, RwLock};
 use warp::{Filter, Rejection};
 
 mod handler;
@@ -15,26 +15,75 @@ pub struct Client {
 }
 
 pub type Clients = Arc<Mutex<HashMap<String, Client>>>;
+pub type FasterClients = Arc<RwLock<HashMap<String, Client>>>;
 
 #[tokio::main]
 async fn main() {
     let clients: Clients = Clients::default();
     initialize_clients(&clients).await;
 
+    let faster_clients: FasterClients = FasterClients::default();
+    initialize_faster_clients(&faster_clients).await;
+
     let read_route = warp::path!("read")
         .and(with_clients(clients.clone()))
         .and_then(handler::read_handler);
 
+    let faster_route = warp::path!("faster")
+        .and(with_clients(clients.clone()))
+        .and_then(handler::faster_read_handler);
+
+    let fast_route = warp::path!("fast")
+        .and(with_faster_clients(faster_clients.clone()))
+        .and_then(handler::fast_read_handler);
+
     println!("Started server at localhost:8080");
-    warp::serve(read_route).run(([0, 0, 0, 0], 8080)).await;
+    warp::serve(read_route.or(faster_route).or(fast_route))
+        .run(([0, 0, 0, 0], 8080))
+        .await;
 }
 
 fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
     warp::any().map(move || clients.clone())
 }
 
+fn with_faster_clients(
+    clients: FasterClients,
+) -> impl Filter<Extract = (FasterClients,), Error = Infallible> + Clone {
+    warp::any().map(move || clients.clone())
+}
+
 async fn initialize_clients(clients: &Clients) {
     let mut clients_lock = clients.lock().await;
+    clients_lock.insert(
+        String::from("87-89-34"),
+        Client {
+            user_id: 1,
+            subscribed_topics: vec![String::from("cats"), String::from("dogs")],
+        },
+    );
+    clients_lock.insert(
+        String::from("22-38-21"),
+        Client {
+            user_id: 2,
+            subscribed_topics: vec![String::from("cats"), String::from("reptiles")],
+        },
+    );
+    clients_lock.insert(
+        String::from("12-67-22"),
+        Client {
+            user_id: 3,
+            subscribed_topics: vec![
+                String::from("mice"),
+                String::from("birds"),
+                String::from("snakes"),
+            ],
+        },
+    );
+}
+
+async fn initialize_faster_clients(clients: &FasterClients) {
+    let mut clients_lock = clients.write().await;
     clients_lock.insert(
         String::from("87-89-34"),
         Client {
